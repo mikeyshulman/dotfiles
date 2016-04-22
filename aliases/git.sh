74f6246583cba55d0e8de12d7fddd72e21225b54
sshleifer <sam@kensho.com>
2016-04-22 14:50:24 -0400

    added vgrep

--- aliases/git.sh
+++ aliases/git.sh
@@ -15,3 +15,7 @@ alias pcheck='git diff master | grep print'
 gday () {
     git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
     }
+
+vgrep () {
+    vim -p `git grep --name-only $1`
+}
