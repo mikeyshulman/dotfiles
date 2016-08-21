a4d7d2c1946f26f6488de4803353abfc1f9ab297
sshleifer <sam@kensho.com>
2016-08-21 19:22:02 -0400

    add kube-cp

--- aliases/chief.sh
+++ aliases/chief.sh
@@ -3,6 +3,14 @@ export GREEN_SERVER='http://10.145.32.1:8080'
 export ORANGE_SERVER='http://10.145.32.1:8080'
 
 
+kube-cp () {  # copy $2 to $1
+        tar zcf - $2 | kubectl exec -i $1 tar zxvf -
+}
+
+cp-orange(){
+    kube-cp orange-notebook-shleifer-5f536ee-wpmhn $1
+}
+
 korange() {
     color='orange'
     k config set-cluster $color --server=$ORANGE_SERVER
