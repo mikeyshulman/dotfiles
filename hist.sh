64dc95f00b644e4809341a67074b74f8af0346a4
sshleifer <sam@kensho.com>
2016-09-09 16:03:23 +0000

    script for user_data stuff

---
+++ hist.sh
@@ -0,0 +1,5 @@
+   cp -R user_data/~.ssh ~
+   cp -R user_data/dotfiles ~
+   # cp -R user_data/public_data public_data
+   # 12  rm -rf public_data
+   echo "run setup from dotfiles, then source ~/.zshrc"
