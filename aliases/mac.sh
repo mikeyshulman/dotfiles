8e82947b86323f4e1aada691db13f50ea318850a
sshleifer <sam@kensho.com>
2016-10-09 15:45:48 -0400

    absolute path to venv

--- aliases/mac.sh
+++ aliases/mac.sh
@@ -5,7 +5,9 @@ if [[ "$OSTYPE" != 'linux-gnu' ]]; then
     # The next line enables shell command completion for gcloud.
     source "${HOME}/google-cloud-sdk/path.zsh.inc"
     source "${HOME}/google-cloud-sdk/completion.zsh.inc"
-    alias klenv='source kml/bin/activate'
+    alias mit='cd ~/Dropbox/MIT/ml/'
+    alias mrepo='cd ~/Dropbox/MIT/ml/'
+    alias klenv='source ~/flow/kensho-learn/kml/bin/activate'
     source ~/flow/zentreefish/.inenv/inenv.sh
     klenv  # source venv
 fi
