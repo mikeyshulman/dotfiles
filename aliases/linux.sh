e2a4a892870197a4c4f19956dc683161bd4f5f8c
sshleifer <sam@kensho.com>
2016-10-12 12:51:24 -0400

    fixed winfo

--- aliases/linux.sh
+++ aliases/linux.sh
@@ -3,7 +3,9 @@ nuc_pip (){
     export PIP_FIND_LINKS=http://${PIP_LINK_HOST}:8080/packages/
     pip install --no-index --trusted-host=${PIP_LINK_HOST} $@
 }
+
 winfo () {
+    echo "stty rows $LINES columns $COLUMNS; reset"
 }
 
 if [[ "$OSTYPE" == 'linux-gnu' ]]; then
