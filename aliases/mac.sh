4a05b652fb35ec38f1aa21ca88072af3100ad41c
sshleifer <sam@kensho.com>
2016-10-21 11:56:52 -0400

    vim updates

--- aliases/mac.sh
+++ aliases/mac.sh
@@ -4,6 +4,7 @@ if [[ "$OSTYPE" != 'linux-gnu' ]]; then
     # The next line updates PATH for the Google Cloud SDK.
     # The next line enables shell command completion for gcloud.
     source "${HOME}/google-cloud-sdk/path.zsh.inc"
+    alias lake="gcloud compute --project kensho-learn ssh --zone us-east1-b data-lake"
     source "${HOME}/google-cloud-sdk/completion.zsh.inc"
     alias mit='cd ~/Dropbox/MIT/ml/repo'
     alias klenv='source ~/flow/kensho-learn/kml/bin/activate'
