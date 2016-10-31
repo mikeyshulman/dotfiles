dbca9526ebd9d267ab05ce448b0d7411dff292db
sshleifer <sam@kensho.com>
2016-10-31 14:59:34 +0000

    updated cp_user_data

--- scripts/cp_user_data.sh
+++ scripts/cp_user_data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+touch ~/.secrets.sh
 cp -R /app/user_data/.secrets.sh ~
 cp -R /app/user_data/.ssh ~
 cp -R /app/user_data/dotfiles ~
 cp -R /app/user_data/.vim ~
 cd ~/dotfiles
 ./setup.sh
+git pull origin master
 source ~/.zshrc
